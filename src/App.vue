1ac7eaa5ce7ad0942af246e5d7691bd293529f6f
Victoria065 <3361182796@qq.com>
2026-06-28 10:07:09 +0800

    day2: complete router, navigation and element-plus config

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,27 @@
 
 <template>
   <main class="app">
-    <h1>校园轻集市</h1>
-    <p>AI 辅助前端工程实践种子项目</p>
-    <RouterView />
+    <el-menu mode="horizontal" style="margin-bottom: 24px;">
+      <el-menu-item index="/home">
+        <router-link to="/home">首页</router-link>
+      </el-menu-item>
+      <el-menu-item index="/list">
+        <router-link to="/list">列表</router-link>
+      </el-menu-item>
+      <el-menu-item index="/publish">
+        <router-link to="/publish">发布</router-link>
+      </el-menu-item>
+      <el-menu-item index="/message">
+        <router-link to="/message">消息</router-link>
+      </el-menu-item>
+      <el-menu-item index="/profile">
+        <router-link to="/profile">我的</router-link>
+      </el-menu-item>
+      <el-menu-item index="/board">
+        <router-link to="/board">看板</router-link>
+      </el-menu-item>
+    </el-menu>
+    <router-view />
   </main>
 </template>
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,48 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      redirect: '/home'
     },
-  ],
+    {
+      path: '/home',
+      name: 'home',
+      component: () => import('@/views/HomeView.vue')
+    },
+    {
+      path: '/list',
+      name: 'list',
+      component: () => import('@/views/ListView.vue')
+    },
+    {
+      path: '/detail/:id',
+      name: 'detail',
+      component: () => import('@/views/DetailView.vue')
+    },
+    {
+      path: '/publish',
+      name: 'publish',
+      component: () => import('@/views/PublishView.vue')
+    },
+    {
+      path: '/message',
+      name: 'message',
+      component: () => import('@/views/MessageView.vue')
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: () => import('@/views/ProfileView.vue')
+    },
+    {
+      path: '/board',
+      name: 'board',
+      component: () => import('@/views/BoardView.vue')
+    }
+  ]
 })
 
 export default router
